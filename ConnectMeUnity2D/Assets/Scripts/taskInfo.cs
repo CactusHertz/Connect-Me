1f2169581e6aec5c3e9c6c64b039bcf04f44ec09
Eon24 <50159128+Eon24@users.noreply.github.com>
2021-10-31 15:21:03 -0700

    fixed issue where assigned sockets for a task were identical

--- ConnectMeUnity2D/Assets/Scripts/taskInfo.cs
+++ ConnectMeUnity2D/Assets/Scripts/taskInfo.cs
@@ -11,7 +11,7 @@ public class taskInfo : MonoBehaviour
     [SerializeField] GameObject textObject;
 
     // Might change this naming convention for the socket, it is such a hassle to update.
-    string[] socketNameList = { "A1", "A2", "A3", "A4",  "A5", "A6", "A7", "A8", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "A9", "A10", "A11", "A12",  "A13", "A14", "A15", "A16", "B9", "B10", "B11", "B12", "B13", "B14", "B15", "B16"};
+    string[] socketNameList = { "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "A9", "A10", "A11", "A12",  "A13", "A14", "A15", "A16", "B9", "B10", "B11", "B12", "B13", "B14", "B15", "B16"};
 
     
     // Start is called before the first frame update
@@ -20,10 +20,17 @@ public class taskInfo : MonoBehaviour
         // add check for if a slot is already taken by another task.
         // can only be overidden by an emergency call
         tags[0] = Random.Range(0, 32);
-        tags[1] = Random.Range(0, 32);
-
+        bool invalidRan = true;
+        while (invalidRan == true)
+        {
+            int ran2 = Random.Range(0, 32);
+            if (tags[0] != ran2)
+            {
+                tags[1] = ran2;
+                invalidRan = false;
+            }
+        }
         textObject.GetComponent<TextMesh>().text = (socketNameList[tags[0]] + " -> " + socketNameList[tags[1]]);
- 
     }
 
     // Update is called once per frame
